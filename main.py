c3547ee4375cd70a0ff640ed15eb69fd4e8ea421
Daria Andreea <daria@vav.ro>
2020-03-02 22:00:12 -0500

    Steps 001, 002, and 003

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("Welcome to THe Heart-Hands of the Pants")
+print("Welcome to The Heart-Hands and the Pants!\n")
+
 avatar = input("Enter your avatar name here: \n")
+
+print("Gretting,", avatar, "! The kingdom of pants has been conquered by the evil hat masterminds! Save the pant people and defeat the evil hat wizard! May the odds be with you Heart-Hand!\n")
+
+input('Please press enter to continue:\n')
+
+print("Each boss you encounter will have a certain amount of health points or HP, which determines how hard the boss is. In order to defeat your opponets you have to attck them. You have 10 tries to defeat your enemny and each hit is a random number between 0 and 10 (there is a higher probibility that you will get higher numbers than lower ones) Everytime you finish a turn, the boss will also attack you using the same fighting system except as the bosses increase, their attcks also become greater.\n")
+
+input('Are you ready to begin your mission? (Press Enter)\n')
+
+input("You've entered the Hair Zone...\n")
+
+input("Watch out! Beanie blocks the way! This boss has 5 HP! \n")
+
+print("Wassup my dudes? You lookin' pretty rad fam but what makes you think you are worthy to defeat me! Ha! There's no chance I'm letting you get past me radical broski!\n")
+
+# Find a way to make the player press enter without using input oof
