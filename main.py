90d579b14976db4737a5f1b6db6d3301f1f17c4f
Daria Andreea <daria@vav.ro>
2020-03-11 19:31:16 -0400

    Introduced first boss and finished intro

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import time
+import attack_points
+
 print("Welcome to The Heart-Hands and the Pants!\n")
 
 avatar = input("Enter your avatar name here: \n")
 
-print("Gretting,", avatar, "! The kingdom of pants has been conquered by the evil hat masterminds! Save the pant people and defeat the evil hat wizard! May the odds be with you Heart-Hand!\n")
+print("Gretting,", avatar, "!\nThe kingdom of pants has been conquered by the evil hat masterminds!\nSave the pant people and defeat the evil hat wizard! \nMay the odds be with you Heart-Hand!")
 
-input('Please press enter to continue:\n')
+time.sleep(6)
 
-print("Each boss you encounter will have a certain amount of health points or HP, which determines how hard the boss is. In order to defeat your opponets you have to attck them. You have 10 tries to defeat your enemny and each hit is a random number between 0 and 10 (there is a higher probibility that you will get higher numbers than lower ones) Everytime you finish a turn, the boss will also attack you using the same fighting system except as the bosses increase, their attcks also become greater.\n")
+print("How to Play:\nEach boss you encounter will have a certain amount of health points or HP, which determines how hard the boss is.\nIn order to defeat your opponets you have to attck them.\nYou have 10 tries to defeat your enemny and each hit is a random number between 0 and 10.\nThere is a higher probibility that you will get higher numbers than lower ones.\nEverytime you finish a turn, the boss will also attack you using the same fighting system except as the bosses increase, their attcks also become greater.\n")
 
 input('Are you ready to begin your mission? (Press Enter)\n')
 
-input("You've entered the Hair Zone...\n")
+print("You've entered the Hair Zone...\n")
 
-input("Watch out! Beanie blocks the way! This boss has 5 HP! \n")
+time.sleep(1.5)
+
+print("Watch out! Beanie blocks the way! This boss has 5 HP! \n")
+
+time.sleep(3)
 
 print("Wassup my dudes? You lookin' pretty rad fam but what makes you think you are worthy to defeat me! Ha! There's no chance I'm letting you get past me radical broski!\n")
-
-# Find a way to make the player press enter without using input oof
