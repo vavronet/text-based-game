3bf02eb0093526093e285564810d2156f52ca727
Daria Andreea <daria@vav.ro>
2020-04-13 19:50:09 -0400

    004 - Created configuration file

---
+++ config.py
@@ -0,0 +1,13 @@
+points = {
+    'player': [0, 1, 3, 5, 5, 6, 9, 10, 10],
+    'first_boss': [0, 0, 1, 1, 1, 2, 2, 3, 4, 5],
+    'second_boss': [0, 1, 1, 2, 3, 3, 4, 4, 6, 7],
+    'final_boss': [1, 2, 2, 4, 5, 6, 7, 8, 9, 10]
+}
+
+health = {
+    'player': 20,
+    'first_boss': 10,
+    'second_boss': 15,
+    'final_boss': 20
+}
