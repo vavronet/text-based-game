d80ceeeb8c417dc0855686b4584726596fde238b
Daria Andreea <daria@vav.ro>
2020-04-21 20:37:39 -0400

    005 - Complete code for first boss

--- first_boss.py
+++ first_boss.py
@@ -1,4 +1,59 @@
-def run():
-    print('Here comes the code for first boss module')
+import time
+import config
+import random
 
-    return True
+def run():
+    first_boss_intro()
+
+    game_over = False
+    status = False
+    tries = config.tries['first_boss']
+    player_health = config.health['player']
+    boss_health = config.health['first_boss']
+
+    while game_over == False:
+        if tries == 0:
+            game_over = True
+            status = False
+        else:
+            if player_health <= 0:
+                game_over = True
+                status = False
+            else:
+                # get random hit points for player
+                player_points = random.choice(config.points['player'])
+                # calculate boss health
+                boss_health = boss_health - player_points
+                # decrease # of tries
+                tries = tries - 1
+                # print attack details
+                player_attack_details = "PLAYER attack: \n--------------\nPoints: {}, Boss health: {}, Tries left: {}"
+                print(player_attack_details.format(player_points, boss_health, tries))
+                input("Press any key to continue...\n")
+                
+                if boss_health <= 0:
+                    game_over = True
+                    status = True
+                else:
+                    # get random hit points for boss
+                    boss_points = random.choice(config.points['first_boss'])
+                    # calculate player health
+                    player_health = player_health - boss_points
+                    # print boss attack details
+                    boss_attack_details = "\nBOSS attack: \n------------\nPoints: {}, Player health: {}"
+                    print(boss_attack_details.format(boss_points, player_health))
+                    input("Press any key to continue...\n")
+    else:
+        if status == False:
+            print("You Lost!\n")
+        else:
+            print("You defeated the first boss!\n")
+
+    return status
+
+def first_boss_intro():
+    print("You've entered the Hair Zone... Watch out! Beanie blocks the way!\n")
+    time.sleep(1.5)
+    print('You have {} health points.'.format(config.health['player']))
+    print('This boss has {} health points!'.format(config.health['first_boss']))
+    print('You have {} tries to defeat him!\n'.format(config.tries['first_boss']))
