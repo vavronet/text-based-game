3cab89f8edad9f587e01652b248f138cd9b7aad1
Daria Andreea <daria@vav.ro>
2020-04-21 20:36:19 -0400

    Clean up game intro

--- game_intro.py
+++ game_intro.py
@@ -5,20 +5,12 @@ def run():
 
     avatar = input("Enter your avatar name here: \n")
 
-    print("Gretting,", avatar, "!\nThe kingdom of pants has been conquered by the evil hat masterminds!\nSave the pant people and defeat the evil hat wizard! \nMay the odds be with you Heart-Hand!")
+    print("\nGretting, {}!\nThe kingdom of pants has been conquered by the evil hat masterminds!\nSave the pant people and defeat the evil hat wizard! \nMay the odds be with you Heart-Hand!".format(avatar))
 
-    time.sleep(6)
+    time.sleep(3)
 
     print("How to Play:\nEach boss you encounter will have a certain amount of health points or HP, which determines how hard the boss is.\nIn order to defeat your opponets you have to attck them.\nYou have 10 tries to defeat your enemny and each hit is a random number between 0 and 10.\nThere is a higher probibility that you will get higher numbers than lower ones.\nEverytime you finish a turn, the boss will also attack you using the same fighting system except as the bosses increase, their attcks also become greater.\n")
 
     input('Are you ready to begin your mission? (Press Enter)\n')
 
-    print("You've entered the Hair Zone...\n")
-
-    time.sleep(1.5)
-
-    print("Watch out! Beanie blocks the way! This boss has 5 HP! \n")
-
-    time.sleep(3)
-
-    print("Wassup my dudes? You lookin' pretty rad fam but what makes you think you are worthy to defeat me! Ha! There's no chance I'm letting you get past me radical broski!\n")
+    
