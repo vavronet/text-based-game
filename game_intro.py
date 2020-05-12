a9b3f36836e9252c1a7766b2b629b2109813274d
Daria Andreea <daria@vav.ro>
2020-05-12 14:56:22 -0400

    Made game intro easier to read

--- game_intro.py
+++ game_intro.py
@@ -9,7 +9,15 @@ def run():
 
     time.sleep(3)
 
-    print("How to Play:\nEach boss you encounter will have a certain amount of health points or HP, which determines how hard the boss is.\nIn order to defeat your opponets you have to attck them.\nYou have 10 tries to defeat your enemny and each hit is a random number between 0 and 10.\nThere is a higher probibility that you will get higher numbers than lower ones.\nEverytime you finish a turn, the boss will also attack you using the same fighting system except as the bosses increase, their attcks also become greater.\n")
+    print("How to Play:\nEach boss you encounter will have a certain amount of health points or HP, which determines how hard the boss is.\n")
+   
+    time.sleep(1)
+   
+    print("In order to defeat your opponets you have to attck them.\nYou have 10 tries to defeat your enemny and each hit is a random number between 0 and 10.\nThere is a higher probibility that you will get higher numbers than lower ones.")
+
+    time.sleep (1)
+
+    print("\nEverytime you finish a turn, the boss will also attack you using the same fighting system except as the bosses increase, their attcks also become greater.\n")
 
     input('Are you ready to begin your mission? (Press Enter)\n')
 
